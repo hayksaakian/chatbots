e78cd45b539fa9669fd4300a5f3d4e5d92788933
hayksaakian <fakest.id.ever@gmail.com>
2014-02-26 02:51:23 -0800

    proper attr_accessor

--- doge_fetcher.rb
+++ doge_fetcher.rb
@@ -15,7 +15,7 @@ APP_ROOT = File.expand_path(File.dirname(__FILE__))
 CACHE_FILE = APP_ROOT+"/cache/"
 
 class DogeFetcher
-  attr_accesor :regex
+  attr_accessor :regex
   def initialize
     @regex = /^!(doge|dgc| SoDoge|SoDoge)/i
   end
