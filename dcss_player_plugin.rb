504d3e6df0bd7fdab0ff5d129b7b5dd288030c0c
hayksaakian <fakest.id.ever@gmail.com>
2014-03-01 11:30:49 -0800

    constantize

--- dcss_player_plugin.rb
+++ dcss_player_plugin.rb
@@ -3,19 +3,19 @@ require 'cinch'
 
 require_relative 'dcss_player'
 
-fetcher = DcssPlayer.new
+FETCHER = DcssPlayer.new
 
 class DcssPlayerPlugin
   include Cinch::Plugin
-  match fetcher.regex
+  match FETCHER.regex
 
   def check(query)
-    fetcher.check(query)
+    FETCHER.check(query)
   end
 
   def execute(m, query)
-    if fetcher.ready
-      result = fetcher.check(p_message)
+    if FETCHER.ready
+      result = FETCHER.check(p_message)
       if !result.nil? and result.length > 0
         result << suffix
         m.reply result
