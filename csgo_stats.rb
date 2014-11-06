6ee052b029f39b4b26c14cc707d8bd5428f6f8f2
Hayk Saakian <fakest.id.ever@gmail.com>
2014-11-05 20:06:51 -0800

    prototyped !csgo

---
+++ csgo_stats.rb
@@ -0,0 +1,101 @@
+require 'rubygems'
+require 'net/http'
+require 'open-uri'
+require 'json'
+require 'nokogiri'
+require 'cgi'
+require 'digest'
+require 'action_view'
+include ActionView::Helpers::DateHelper
+
+ENDPOINT = "http://csgo-stats.com/llllIIIllIIIlIIIIlllIIII/?ajax&uptodate"
+HUMAN_LINK = "http://csgo-stats.com/llllIIIllIIIlIIIIlllIIII/"
+VALID_WORDS = %w{cs counterstrike ayyylmao}
+RATE_LIMIT = 16 # seconds
+CACHE_DURATION = 60 #seconds
+APP_ROOT = File.expand_path(File.dirname(__FILE__))
+CACHE_FILE = APP_ROOT+"/cache/"
+
+class CsgoStats
+  attr_accessor :regex
+  def initialize
+    @regex = /^!(#{VALID_WORDS.join('|')})/i
+  end
+  def ready
+    last_time = @last_time || 0
+    now = Time.now.to_i
+    if now - last_time > RATE_LIMIT
+      @last_time = now
+      return true
+    end
+    return false
+  end
+  def check(query)
+    return trycheck(query)
+  rescue Exception => e
+    puts e.message
+    m = e.message
+    m << "\n\n --->"
+    m << e.backtrace.join("\n|\n")
+    " AYYYLMAO tell hephaestus something broke. Exception: #{m.to_s}"
+  end
+  def trycheck(query)
+    cached = getcached(ENDPOINT)
+    # expire cache if...
+    if cached.nil? or cached["date"] < Time.now.to_i - CACHE_DURATION
+      jsn = getjson(ENDPOINT)
+      if jsn.nil?
+        raise "Failed to GET CSGO data from csgo-stats.com"
+      else
+        setcached(ENDPOINT, jsn)
+      end
+    else
+      jsn = cached
+    end
+
+    parsed_html = Nokogiri.html(jsn["content"])
+    lastmatch = parsed_html.css("#lastmatch")
+    lmtxt = lastmatch.children[3].text()
+    result = lmtxt.include?('Win') ? 'won' : 'lost'
+    if lmtxt.include?('.')
+      lmtxt = lmtxt.split('.')[0]
+    end
+    # contains lifetime stats
+    misc_data = parsed_html.css('#misc').children[3].children[3]
+    # matches won / played
+    overall = "#{misc_data.children[15]} / #{misc_data.children[11]}"
+    return "Destiny #{result} a game with #{lmtxt} (#{overall} wins overall) #{HUMAN_LINK}"
+    # output << " as of "
+    # output << time_ago_in_words(Time.at(jsn["date"]))
+    # output << " ago"
+    # # output << " i finished checking at "+time_ago_in_words(Time.now)
+    # return output
+  end
+
+  def getjson(url)
+    content = open(url).read
+    return JSON.parse(content)
+  end
+
+  # safe cache! won't die if the bot dies
+  def getcached(url)
+    return @cached_json if !@cached_json.nil?
+    path = CACHE_FILE + hashed(url) + ".json"
+    if File.exists?(path)
+      f = File.open(path)
+      return JSON.parse(f.read)
+    end
+    return nil
+  end
+  def setcached(url, jsn)
+    @cached_json = jsn
+    path = CACHE_FILE + hashed(url) + ".json"
+    File.open(path, 'w') do |f2|
+      f2.puts JSON.unparse(jsn)
+    end
+  end
+
+  def hashed(url)
+    return Digest::MD5.hexdigest(url).to_s
+  end
+end
