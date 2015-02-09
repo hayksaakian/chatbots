7badb5c8d466b53b4c99a31fa272c101622a0e8a
Hayk Saakian <fakest.id.ever@gmail.com>
2015-02-09 13:51:28 -0800

    space

--- broadcaster.rb
+++ broadcaster.rb
@@ -36,7 +36,7 @@ class Broadcaster
   def pickone
     phrases = [
       "#{ENDPOINT} WORTH $#{MINIMUM.to_s} to get your Message read by Robot Lady",
-      "Click for DANKMEMES: #{ENDPOINT}  Robot Lady will read messages on donations over than #{MINIMUM.to_s} dollars",
+      "Click for DANKMEMES : #{ENDPOINT}  Robot Lady will read messages on donations over than #{MINIMUM.to_s} dollars",
       "Min. $#{MINIMUM.to_s} donation for Robot Lady to read it! Donation Link: #{ENDPOINT}",
       "Robot Lady will read your DANKMEMES for at least $#{MINIMUM.to_s} donations. Donate Here: #{ENDPOINT}"
     ]
